2fb2e9f586c888a14df689eb94c8feee72788ac0
Ashish-sarmah <102532603+Ashish-sarmah@users.noreply.github.com>
2023-12-28 09:56:36 +0530

    Update calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,30 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor (){
+    this.result = 0;}
+
+  add (num){
+  this.result += num}
+  subtract (num){
+  this.result -= num}
+  multuply (num){
+  this.result *= num}
+  divide (num){
+  this.result /= num}
+  clear (){
+  this.result =0}
+  getResult (){
+  return this.result}
+
+  calculate(str){
+     this.result = new Function('return ' + str)()
+  }
+}
+
+const ans = new Calculator();
+ans.calculate(`10 +   2 *    (   6 - (4 + 1) / 2) + 7`);
+console.log(ans.getResult())
 
 module.exports = Calculator;
